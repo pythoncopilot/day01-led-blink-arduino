1ea46704e7f398e7392ecf5e54bdd17f9088b143
pythoncopilot <147984499+pythoncopilot@users.noreply.github.com>
2026-06-05 23:36:37 +0200

    Refactor LED control to use LED_BUILTIN

--- main.ino
+++ main.ino
@@ -1,11 +1,22 @@
 void setup() {
-  pinMode(13, OUTPUT);
+
+  // ===== STAGE 1: SERIAL START =====
+  Serial.begin(9600);
+  Serial.println("Hello World - Arduino Started");
+
+  // ===== STAGE 2: LED SETUP =====
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(13, HIGH);
+
+  // ===== STAGE 3: LED ON =====
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("LED STATUS: ON");
   delay(1000);
 
-  digitalWrite(13, LOW);
+  // ===== STAGE 3: LED OFF =====
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("LED STATUS: OFF");
   delay(1000);
 }
